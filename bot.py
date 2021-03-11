dbe64e1e214158d1a8d98085c3761251603ea4d7
knifeworm <knifeworm213133@gmail.com>
2021-03-11 20:58:15 +0000

    Modified bot.py
    Modified bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
 import discord
 from discord.ext import commands, tasks
+from secret import TOKEN
+from secret import PREFIX
 
-bot = commands.Bot(command_prefix = "n!")
+bot = commands.Bot(command_prefix = PREFIX)
 
 @bot.event
 async def on_ready():
     print("New Folder is online!")
+
+
+bot.run(TOKEN)
