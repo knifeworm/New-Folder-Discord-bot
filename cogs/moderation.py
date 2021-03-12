3d7b14f60796db908dde71db54be6e4413d570a3
knifeworm <knifeworm213133@gmail.com>
2021-03-12 16:21:15 +0000

    Editied Moderation.py Added Binary files
    Editied Moderation.py Added Binary files

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -5,9 +5,6 @@ class moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def test(self, ctx):
-        await ctx.send("Yes")
 
 def setup(bot):
     bot.add_cog(moderation(bot))
